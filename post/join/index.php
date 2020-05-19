ddbcd60dd58cc25c50b6e378169286214a3f4d8f
yoyo629 <king.crimson.723.yu@gmail.com>
2020-05-20 00:00:20 +0900

    chapter6-4終了

--- post/join/index.php
+++ post/join/index.php
@@ -1,3 +1,46 @@
+<?php
+session_start();
+
+//フォーム送信されたかどうかの確認処理
+if (!empty($_POST)) {
+    //エラー項目の確認（会員登録フォームに入力された内容が空かどうか判定）
+    if ($_POST['name'] === '') {
+        $error['name'] = 'blank';
+    }
+    if ($_POST['email'] === '') {
+        $error['email'] = 'blank';
+    }
+    //パスワードが4文字以下の時にエラー処理
+    if (strlen($_POST['password']) < 4) {
+        $error['password'] = 'length';
+    }
+    if ($_POST['password'] === '') {
+        $error['password'] = 'blank';
+    }
+    //画像ファイルチェック
+    $fileName = $_FILES['image']['name'];
+    if (!empty($fileName)) {
+        $ext = substr($fileName, -3);//後ろから3文字を切り出して拡張子の確認
+        if ($ext != 'jpg' && $ext != 'gif') {
+            $error['image'] === 'type';
+        }
+    }
+    
+    if (empty($error)) {
+        //画像アップロード
+        $image = date('YmsHis') . $_FILES['image']['name'];
+        move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' .$image);
+
+        $_SESSION['join'] = $_POST;
+        $_SESSION['join']['name'] = $image;
+        header('Location: check.php');
+        exit();
+    }   
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,20 +54,47 @@
 <body>
 <div id="wrap">
   <div id="head">
-    <h1>ひとこと掲示板</h1>
+    <h1>会員登録</h1>
   </div>
   <div id="content">
     <p>次のフォームに必要事項を入力してください。</p>
     <form action="" method="post" enctype="multipart/form-data">
         <dl>
             <dt>ニックネーム<span class="required">必要</span></dt>
-            <dd><input type="text" name="name" size="35" maxlength="255" /></dd>
+            <dd>
+                <input type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['name'], ENT_QUOTES); ?>" />
+                <?php if($error['name'] === 'blank'): ?>
+                <p class="error"> *　ニックネームを入力してください</p>
+                <?php endif; ?>
+            </dd>
             <dt>メールアドレス<span class="required">必要</span></dt>
-            <dd><input type="text" name="email" size="35" maxlength="255" /></dd>
+            <dd>
+                <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>" />
+                <?php if($error['email'] === 'blank'): ?>
+                <p class="error"> *　メールアドレスを入力してください</p>
+                <?php endif; ?>
+            </dd>
             <dt>パスワード<span class="required">必要</span></dt>
-            <dd><input type="text" name="password" size="10" maxlength="20" /></dd>
+            <dd>
+                <input type="text" name="password" size="10" maxlength="20" value="<?php echo htmlspecialchars($_POST['password'],ENT_QUOTES); ?>" />
+                <?php if($error['password'] === 'blank'): ?>
+                <p class="error"> *　パスワードを入力してください</p>
+                <?php endif; ?>
+                <?php if ($error['password'] === 'length'): ?>
+                <p class="error"> *　パスワードは4文字以上です。</p>   
+                <?php endif; ?>
+            </dd>
             <dt>写真など</dt>
-            <dd><input type="file" name="image" size="35" /></dd>
+            <dd>
+                <input type="file" name="image" size="35" />
+                <?php if ($error['image'] == 'type'): ?>
+                <p class="error"> *　写真などは「.gif」「.jpg」の画像を
+                指定してください</p>
+                <?php endif; ?>
+                <?php if (!empty($error)): ?>
+                <p class="error"> *　恐れ入りますが、画像を再指定してください。</p>
+                <?php endif; ?>
+            </dd>
         </dl>
         <div><input type="submit" value="入力内容を確認する" /></div>
     </form>        
