8d709369d89262863ab6f3a30cc8202f4ff48470
yoyo629 <king.crimson.723.yu@gmail.com>
2020-05-27 00:02:07 +0900

    掲示板完成

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;charset=utf8','root','root');
+    $db = new PDO('mysql:dbname=mini_bbs;host=localhost;charset=utf8','root','root');
 } catch (PDOExeption $e) {
     echo 'DB接続エラー : ' . $e->getMessage();
 }
---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if(isset($_SESSION['id'])) {
+    $id = $_REQUEST['id'];
+
+    // 投稿を検査する
+    $message = $db->prepare('SELECT * FROM posts WHERE id = ?');
+    $message->execute(array($id));
+    $message = $message->fetch();
+
+    if ($message['member_id'] == $_SESSION['id']) {
+        // 削除する
+        $del = $db->prepare('DELETE FROM posts where id=?');
+        $del->execute(array($id));
+    }
+}
+
+header('Location: index.php'); exit();
+?>
