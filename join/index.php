74f725f65b4d173203617a1bfb2d4c44b499545f
yoyo629 <king.crimson.723.yu@gmail.com>
2020-06-30 01:11:10 +0900

    会員登録画面のcss反映

--- join/index.php
+++ join/index.php
@@ -70,7 +70,7 @@ if ($_REQUEST['action'] == 'rewrite') {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>ひとこと掲示板</title>
-	<link rel="stylesheet" href="../style.css" />
+	<link rel="stylesheet" href="../css/style.css" />
 </head>
 
 <body>
