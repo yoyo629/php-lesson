afe0245a2b8d6647a7a640528561d71033171b13
yoyo629 <king.crimson.723.yu@gmail.com>
2020-07-01 12:02:39 +0900

    check.php修正

--- join/check.php
+++ join/check.php
@@ -58,7 +58,7 @@ if (!empty($_POST)) {
             </dd>
             <dt>写真など</dt>
             <dd>
-            <img src="../member_picture/<?php echo $_SESSION['join']['image']; ?>" width="100" height="100" alt="" />
+            <img src="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES); ?>" width="100" height="100" alt="" />
             </dd>
         </dl>
         <div><a href="index.php?action=rewrite"><&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
